6fc4c05415e63517b6dd0059675ad1a28bd27966
S566474 <142925730+S566474@users.noreply.github.com>
2023-11-01 20:14:38 -0500

    choose added

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Door',DoorRouter);
-app.use('/board',boardRouter);
+app.use('/Door', DoorRouterRouter);
+app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
